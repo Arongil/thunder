d547d944300667fa3e39b72c48e32318617aacfc
Laker Newhouse <lakern@mit.edu>
2024-12-08 23:01:00 -0500

    Transposed copying works (we think?)

--- symmul/symmul.cu
+++ symmul/symmul.cu
@@ -11,7 +11,7 @@ struct symmul_layout {
     using  global_layout  = gl<bf16, 1, 1, -1, -1, base_tile>;
     struct globals        { global_layout A, B, C; };
     struct input_block    { base_tile a[M_BLOCK], b[N_BLOCK]; };  // outer product input
-    struct finish_block   { base_tile c[M_BLOCK][N_BLOCK]; base_tile c_transposed[N_BLOCK][M_BLOCK]; };     // outer product result
+    struct finish_block   { base_tile c[M_BLOCK][N_BLOCK]; };     // outer product result
     struct common_state   { int2 coord; };
     struct consumer_state { rt_fl<16, N_BLOCK*base_tile::cols> accum; rt_bf<16, N_BLOCK*base_tile::cols> accum_bf16; rt_bf<N_BLOCK*base_tile::cols, 16> accum_transposed; }; // register_tile of size 16 by N_BLOCK * however many columns we're doing
 };
@@ -20,7 +20,7 @@ struct symmul_template {
     static constexpr int M_BLOCK = _M_BLOCK, N_BLOCK = _N_BLOCK, SUPER_M = _SUPER_M;
     using layout    = symmul_layout<M_BLOCK, N_BLOCK>;
     using wide_tile = st_bf<64, 64*N_BLOCK>;  // 64 rows by 64*N_BLOCK columns
-    using tall_tile = st_bf<64*M_BLOCK, 64>;  // 64*M_BLOCK rows by 64 columns
+    using tall_tile = st_bf<64*N_BLOCK, 16>;  // 64*N_BLOCK rows by 16 columns
     static constexpr int NUM_CONSUMER_WARPS=M_BLOCK*4, INPUT_PIPE_STAGES=4, PRODUCER_BARRIER_ARRIVALS=1;
     // Helper functions
     template<bool PERISISTENT_GRID=true> __host__ static inline dim3 grid(int M, int N) {
@@ -46,13 +46,14 @@ struct symmul_template {
             return;
         }
         // Skip upper triangular blocks
-        if (args.common.coord.x > args.common.coord.y) {
-            args.num_iters = -1;
-            return;
-        }
         args.num_iters = args.globals.A.cols/64;
         int id = warpgroup::groupid() == NUM_CONSUMER_WARPS/4 ? 0 : warpgroup::groupid(); // producer sets as 0
         args.common.coord = { args.common.coord.x*M_BLOCK + id, args.common.coord.y*N_BLOCK };
+
+        if (args.common.coord.x < args.common.coord.y) {
+            args.num_iters = -1;
+            return;
+        }
     }
     struct producer {
         __device__ static void setup(producer_setup_args<layout> args) {
@@ -103,18 +104,17 @@ struct symmul_template {
             // store the upper triangular part -- first convert to bf16 so that we can tranpose
             kittens::copy(args.state.accum_bf16, args.state.accum);
             kittens::transpose_sep(args.state.accum_transposed, args.state.accum_bf16);
+            auto &c_transposed = reinterpret_cast<layout::base_tile(&)[N_BLOCK][M_BLOCK]>(args.finish.c);
 
-            //warpgroup::store(reinterpret_cast<wide_tile&>(args.finish.c_transposed[warpgroup::groupid()]), args.state.accum_transposed);
-            /*
+            warpgroup::store(reinterpret_cast<tall_tile&>(c_transposed[warpgroup::groupid()]), args.state.accum_transposed);
             warpgroup::sync(warpgroup::groupid()+4);
             if(warpgroup::warpid() == 0) {
                 for(int i = 0; i < N_BLOCK; i++) {
-                    tma::store_async(args.globals.C, args.finish.c[warpgroup::groupid()][i],
+                    tma::store_async(args.globals.C, c_transposed[i][warpgroup::groupid()],
                                                 {args.common.coord.y+i, args.common.coord.x});
                     tma::store_async_read_wait(); // wait that store is finished before reusing finish memory
                 }
             }
-            */
 
             zero(args.state.accum);
             zero(args.state.accum_transposed);
